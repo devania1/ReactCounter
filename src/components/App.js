791e978ee746df2c092f71592a30c54367c66a09
Anna Sobanska <ania.webdev@gmail.com>
2021-08-25 22:08:01 +0200

    add App component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+    const myItem = "item3";
+
+    return (
+        <ul>
+            <li>item1</li>
+            <li>item2 {myItem}</li>
+            <li>{myItem.toUpperCase()}</li>
+        </ul>
+    )
+}
+
+export default App;
